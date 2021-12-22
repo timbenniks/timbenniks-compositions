7c732cea21cc36958c7caeebc5c7c6c065944d3d
Tim Benniks <tbenniks@gmail.com>
2021-12-22 14:07:37 +0100

    cleanup

--- enhancers/index.js
+++ enhancers/index.js
@@ -8,32 +8,16 @@ import { CANVAS_PRISMIC_PARAMETER_TYPES } from "@uniformdev/canvas-prismic";
 import { bigCommerceEnhancer } from "./bigCommerce/bigCommerceEnhancer";
 import { contentfulEnhancer } from "./contentful/contentfulEnhancer";
 import { contentfulQueryEnhancer } from "./contentful/contentfulQueryEnhancer";
-import { blogPostEnhancer } from "./contentful/blogPostEnhancer";
-import { talkEnhancer } from "./contentful/talkEnhancer";
-import { listEnhancer } from "./contentful/listEnhancer";
-import { latestItemEnhancer } from "./contentful/latestItemEnhancer";
 import { contentfulQueryModelConverter } from "./contentful/contentfulQueryModelConverter";
 import { prismicEnhancer } from "./prismic/prismicEnhancer";
 
 export const enhancers = new EnhancerBuilder()
-  .component("latestBlogposts", (latestBlogposts) =>
-    latestBlogposts.data("posts", blogPostEnhancer)
-  )
-  .component("latestTalks", (latestTalks) =>
-    latestTalks.data("talks", talkEnhancer)
-  )
-  .component("latestThing", (latestThing) =>
-    latestThing.data("item", latestItemEnhancer)
-  )
-  .component("list", (list) => list.data("list", listEnhancer))
   .parameterType(CANVAS_BIGCOMMERCE_PARAMETER_TYPES, bigCommerceEnhancer())
   .parameterType(CANVAS_PRISMIC_PARAMETER_TYPES, prismicEnhancer())
-
   .parameterType(
     CANVAS_CONTENTFUL_QUERY_PARAMETER_TYPES,
     compose(contentfulQueryEnhancer(), contentfulQueryModelConverter)
   )
-
   .parameterType(
     CANVAS_CONTENTFUL_PARAMETER_TYPES,
     compose(contentfulEnhancer(), contentfulQueryModelConverter)
