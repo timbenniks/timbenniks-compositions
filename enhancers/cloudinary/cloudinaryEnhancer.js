c065bb9a0d6005cf7c633c2d4c1897a277fc3d4d
Tim Benniks <tbenniks@gmail.com>
2022-03-23 10:51:44 +0100

    Added better cloudinary enhancing

--- enhancers/cloudinary/cloudinaryEnhancer.js
+++ enhancers/cloudinary/cloudinaryEnhancer.js
@@ -1,15 +1,56 @@
 export const CANVAS_CLOUDINARY_PARAMETER_TYPES = "cloudinary-parameter";
 
+function createSrcSet(
+  baseUrl,
+  version,
+  baseOptions,
+  widths,
+  ratio,
+  transformation
+) {
+  const result = [];
+
+  widths.split(",").forEach((width) => {
+    let transforms = `${
+      transformation ? transformation : baseOptions
+    },w_${width}`;
+
+    if (ratio) {
+      transforms = `${
+        transformation ? transformation : baseOptions
+      },ar_${ratio},c_fill,w_${width}`;
+    }
+
+    const url = baseUrl.replace(`v${version}`, transforms);
+
+    result.push(`${url} ${width}w`);
+  });
+
+  return result.join(",");
+}
+
 export const cloudinaryEnhancer = ({ parameter }) => {
   const result = parameter.value.map((media) => {
-    const transformation =
-      media.transformation !== "" ? media.transformation : media.options;
-
     return {
       width: media.width,
       height: media.height,
-      url: media.url.replace(`v${media.version}`, transformation),
+      rawurl: media.url,
+      baseurl: media.url.replace(`v${media.version}`, media.options || ""),
+      transdormedurl: media.transformation
+        ? media.url.replace(`v${media.version}`, media.transformation)
+        : null,
       alt: media.alt,
+      caption: media.caption,
+      srcset: media.widths
+        ? createSrcSet(
+            media.url,
+            media.version,
+            media.options,
+            media.widths,
+            media.ratio || false,
+            media.transformation || false
+          )
+        : null,
     };
   });
 
