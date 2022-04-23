af64d519a83f84f6d600942c1c43436bf3e2247a
Tim Benniks <tbenniks@gmail.com>
2022-04-23 21:22:01 +0200

    added video paths

---
+++ api/video-paths.js
@@ -0,0 +1,16 @@
+import { getClient } from "../enhancers/contentful/client";
+
+module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  const client = getClient();
+  const videos = await client.getEntries({
+    content_type: "video",
+  });
+
+  const paths = videos.items.map((video) => {
+    return `/videos/${video.fields.slug}`;
+  });
+
+  res.end(JSON.stringify({ paths }));
+};
