ebf879823fa132e82e49cc5c8f2c4401d044364e
Tim Benniks <tbenniks@gmail.com>
2022-04-30 10:56:18 +0200

    writings instead of videos

--- api/blog-paths.js
+++ api/blog-paths.js
@@ -4,13 +4,13 @@ module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
   const client = getClient();
-  const videos = await client.getEntries({
+  const writings = await client.getEntries({
     content_type: "blog",
     limit: 200,
   });
 
-  const paths = videos.items.map((video) => {
-    return `/videos/${video.fields.slug}`;
+  const paths = writings.items.map((video) => {
+    return `/writings/${video.fields.slug}`;
   });
 
   res.end(JSON.stringify({ paths }));
