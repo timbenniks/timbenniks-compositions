299269bb404cee8f918081f932af07fb7c3f7cd6
Tim Benniks <tbenniks@gmail.com>
2022-08-03 11:15:01 +0200

    query talks endpoint

---
+++ api/talks.js
@@ -0,0 +1,18 @@
+import { getClient } from "../enhancers/contentful/client";
+import { enhanceItemOutsideEnhancers } from "../enhancers/helpers";
+
+module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  const client = getClient();
+
+  let entries = [];
+
+  entries = await client.getEntries({
+    content_type: "talk",
+  });
+
+  const items = entries.items.map((item) => enhanceItemOutsideEnhancers(item));
+
+  res.end(JSON.stringify({ items }));
+};
