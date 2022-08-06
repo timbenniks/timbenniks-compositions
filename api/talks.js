75729cb295ffb07cbe79c37328746b82e1ce6451
Tim Benniks <tbenniks@gmail.com>
2022-08-06 10:42:37 +0200

    limit amount of talks query

--- api/talks.js
+++ api/talks.js
@@ -1,14 +1,16 @@
+import * as url from "url";
+
 import { getClient } from "../enhancers/contentful/client";
 import { enhanceItemOutsideEnhancers } from "../enhancers/helpers";
 
 module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
-  // const dev = process.env.NODE_ENV === "development";
-  // const link = `${dev ? "http://" : "https://"}${req.headers.host}${req.url}`;
-  // const parsedLink = new url.URL(link);
-  // const params = parsedLink.searchParams;
-  // const limit = params.get("limit") || "";
+  const dev = process.env.NODE_ENV === "development";
+  const link = `${dev ? "http://" : "https://"}${req.headers.host}${req.url}`;
+  const parsedLink = new url.URL(link);
+  const params = parsedLink.searchParams;
+  const limit = params.get("limit") || "";
 
   const client = getClient();
 
@@ -17,7 +19,7 @@ module.exports = async (req, res) => {
   entries = await client.getEntries({
     content_type: "talk",
     order: "-fields.date",
-    // limit: limit ? limit : 100,
+    limit: limit ? limit : 100,
   });
 
   const items = entries.items.map((item) => enhanceItemOutsideEnhancers(item));
